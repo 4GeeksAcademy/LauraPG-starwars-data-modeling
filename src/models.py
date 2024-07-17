91f87fdb6b385afcff024cfb48ca2a903d12efdc
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-07-17 17:15:19 +0000

    comentarios anadidos para una mejora comprensión del back_populates y otros apuntes interesantes

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+#Tabla de usuarios 
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -15,10 +16,14 @@ class Users(Base):
     last_name = Column(String(250))
     email = Column(String(250), nullable=False)
 
+    #RELACIÓN de la tabla users con favourites
     favourites = relationship("Favourites", back_populates="users")
 
     
-
+#Tabla de personajes
+#En la columna gender le aporto un dato tipo Enum:
+#Hace que solo se pueda elegir entre las 3 opciones proporcionadas,
+#que en este caso son male, female y others
 class Characters(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
@@ -30,8 +35,10 @@ class Characters(Base):
     birth_year = Column(String(10))
     gender = Column(Enum('male', 'female', 'others', name='gender'))
 
+    #RELACIÓN de la tabla characters con favourites
     favourites = relationship("Favourites", back_populates="characters")
 
+#Tabla de planetas
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
@@ -44,9 +51,10 @@ class Planets(Base):
     climate = Column(String(50))
     terrain = Column(String(50))
 
+    #RELACIÓN de la tabla planets con favourites
     favourites = relationship("Favourites", back_populates="planets")
 
-
+#Tabla de vehículos
 class Starships(Base):
     __tablename__ = 'starships'
     id = Column(Integer, primary_key=True)
@@ -61,9 +69,12 @@ class Starships(Base):
     max_atmosphering_speed = Column(Integer)
     cargo_capacity = Column(Integer)
 
+    #RELACIÓN de la tabla starhips con favourites
     favourites = relationship("Favourites", back_populates="starships")
 
-
+#Tabla favoritos
+#en esta tabla se añaden 4 columnas que hacen referencia al id de:
+#planetas, vehiculos, personajes y usuarios
 class Favourites(Base):
     __tablename__ = 'favourites'
     id = Column(Integer, primary_key=True)
@@ -72,6 +83,8 @@ class Favourites(Base):
     characters_id = Column(Integer, ForeignKey('characters.id'), nullable=True)
     users_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
+    #Con back_populates, se debe crear explícitamente la RELACIÓN tanto en la clase favourites como en la clases users, starships, planets y characters:
+    #Creando las RELACIONES de la tabla favourites con users, starships, planets y characters
     users = relationship("Users", back_populates="favourites")
     starships = relationship("Starships", back_populates="favourites")
     planets = relationship("Planets", back_populates="favourites")
